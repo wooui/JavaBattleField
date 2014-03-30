f33a7e972775d5193a1be8f2eb1d7df557961af4
wooui <jason.wooui@gmail.com>
2014-03-31 00:00:32 +0800

    JavaMail - get user name and password from authentication dialog

--- src/com/wooui/mail/ReceiveMailTest.java
+++ src/com/wooui/mail/ReceiveMailTest.java
@@ -7,19 +7,22 @@ import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeUtility;
 
 public class ReceiveMailTest {
 	public static void main(String[] args) {
 		Properties props = System.getProperties();
 		String host = "pop3.163.com";
-		String username = "jasonyifei@163.com";
-		String password = "111111";
+	//	String username = "jasonyifei@163.com";
+	//	String password = "111111";
 		String provider = "pop3";
 		
 		try{
-			Session ss = Session.getDefaultInstance(props);
+			//Session ss = Session.getDefaultInstance(props);
+			Session ss = Session.getDefaultInstance(props, new MailAuthenticator());
 			Store store = ss.getStore(provider);
-			store.connect(host, username, password);
+			//store.connect(host, username, password);
+			store.connect(host, null, null);
 			Folder inbox = store.getFolder("INBOX");
 			if(inbox == null){
 				System.out.println("no inbox");
@@ -29,11 +32,11 @@ public class ReceiveMailTest {
 			System.out.println("total email:"+inbox.getMessageCount());
 			Message[] messages = inbox.getMessages();
 			for(Message msg : messages){
-				String from = InternetAddress.toString(msg.getFrom());
+				String from = getChineseFrom(InternetAddress.toString(msg.getFrom()));
 				if(from != null){
 					System.out.println(from);
 				}
-				String replyTo = InternetAddress.toString(msg.getReplyTo());
+				String replyTo = getChineseFrom(InternetAddress.toString(msg.getReplyTo()));
 				if(replyTo != null){
 					System.out.println(replyTo);
 				}
@@ -42,5 +45,20 @@ public class ReceiveMailTest {
 		}catch(Exception e){
 			
 		}
+	}
+	
+	public static String getChineseFrom(String str){
+		String from = str;
+		
+		try{
+			if(from.startsWith("=?GB") || from.startsWith("=?gb") || from.startsWith("=?UTF")){
+				from = MimeUtility.decodeText(from);
+			}else{
+				from = new String(from.getBytes("ISO8859_1"), "GBK");
+			}
+		}catch(Exception e){
+			
+		}
+		return from;
 	}
 }
